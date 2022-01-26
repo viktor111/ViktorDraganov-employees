5e461ffff218e1e7d449ea0c57d995ac791b128a
viktor111 <swifrorlilko@gmail.com>
2022-01-26 17:35:18 +0200

    Add exception handling for file

--- ViktorDraganov-employees/Main.cs
+++ ViktorDraganov-employees/Main.cs
@@ -1,8 +1,8 @@
 ﻿namespace ViktorDraganov_employees
 {
-    using ViktorDraganov_employees.Dtos;
-    using ViktorDraganov_employees.Services;
-
+    using Services;
+    using Services.Exceptions;
+    using ViktorDraganov_employees.Common;
 
     public partial class Main : Form
     {
@@ -12,40 +12,47 @@
         }
 
         private void ImportFileButton_Click(object sender, EventArgs e)
-        {            
+        {
             var fd = new OpenFileDialog();
             if (fd.ShowDialog() == DialogResult.OK)
             {
-                var fileToOpen = fd.FileName;
-
-                var file = new FileInfo(fd.FileName);
-
-                FileNameLabel.Text = file.Name;
-
-                var reader = new StreamReader(fileToOpen);
-
-                var txtReaderService = new TxtFileReaderService(file.FullName);
-                var pairCalculatorService = new PairCalculatorService();
-
-                var lines = txtReaderService.GetLines();
-
-                var pairs = pairCalculatorService.GetPairs(lines);
-
-                for (int i = 0; i < pairs.Count; i++)
+                try
                 {
-                    var pair = pairs[i];
+                    var fileToOpen = fd.FileName;
 
-                    var row = new string[] 
+                    var file = new FileInfo(fd.FileName);
+
+                    FileNameLabel.Text = file.Name;
+
+                    var reader = new StreamReader(fileToOpen);
+
+                    var txtReaderService = new TxtFileReaderService(file.FullName);
+                    var pairCalculatorService = new PairCalculatorService();
+
+                    var lines = txtReaderService.GetLines();
+
+                    var pairs = pairCalculatorService.GetPairs(lines);
+
+                    for (int i = 0; i < pairs.Count; i++)
                     {
+                        var pair = pairs[i];
+
+                        var row = new string[]
+                        {
                         pair.EmployeeId1.ToString(),
                         pair.EmployeeId2.ToString(),
                         pair.ProjectId.ToString(),
                         pair.DaysWorked.ToString()
-                    };
+                        };
 
-                    var listViewItem = new ListViewItem(row);
+                        var listViewItem = new ListViewItem(row);
 
-                    EmployeesListView.Items.Add(listViewItem);
+                        EmployeesListView.Items.Add(listViewItem);
+                    }
+                }
+                catch (BaseException ex)
+                {
+                    MessageBox.Show(ex.Error);
                 }
             }
         }
--- ViktorDraganov-employees/Services/TxtFileReaderService.cs
+++ ViktorDraganov-employees/Services/TxtFileReaderService.cs
@@ -21,13 +21,23 @@
 
             var linesFromFile = this.getLinesFromFile(this.FilePath);
 
+            if (linesFromFile == null)
+            {
+                throw new TxtReaderServiceException("Null line discovered in file!");
+            }
+
             for (int i = 0; i < linesFromFile.Length; i++)
             {
                 var lineString = linesFromFile[i].Split(",");
 
+                if (string.IsNullOrEmpty(lineString[0]) ||
+                    string.IsNullOrEmpty(lineString[1]))
+                {
+                    throw new TxtReaderServiceException("Null or empty elemnt discoverd in file!");
+                }
+
                 var empId = int.Parse(lineString[0]);
                 var projectId = int.Parse(lineString[1]);
-
 
                 var dateFrom = new DateTime();
                 var dateTo = new DateTime();
@@ -50,7 +60,6 @@
                 {
                     dateTo = DateTime.Now;
                 }
-
 
                 var line = new Line(empId, projectId, dateFrom, dateTo);
 
